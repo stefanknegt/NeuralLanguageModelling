68704fb345c3583427732e0ccfd9d71eee492b25
jorism1993 <jorismollinga@hotmail.com>
2017-11-11 16:19:01 +0100

    Add files via upload

--- Data/train_MLP.py
+++ Data/train_MLP.py
@@ -6,6 +6,11 @@ import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
+<<<<<<< HEAD
+import torch.nn.functional as F
+from torch import autograd
+=======
+>>>>>>> master
 from torch.autograd import Variable
 
 outname = "necessary-vectors.txt"
@@ -22,27 +27,30 @@ learning_rate = 0.001
 class Net(nn.Module):
     def __init__(self,input_size,hidden_size, num_classes):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size,hidden_size)
-        self.tanh = nn.Tanh()
-        self.fc2 = nn.Linear(hidden_size,num_classes)
-        self.sigmoid = nn.Sigmoid()
+        self.l1 = nn.Linear(input_size,hidden_size)
+        self.l2 = nn.Linear(hidden_size,num_classes)
 
     def forward(self,x):
-        out = self.fc1(x)
-        out = self.tanh(out)
-        out = self.fc2(out)
-        out = self.sigmoid(out)
-        return out
+        x = self.l1(x)
+        x = F.tanh(x)
+        x = self.l2(x)
+        x = F.softmax(x)
+        return x
 
 mlp = Net(input_size,hidden_size,num_classes)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(mlp.parameters(), lr=learning_rate)
 
+
+print ('word_list length is',len(word_list))
+print ('len of w2i is',len(w2i))
+
 for i in range(N-1,len(word_list)):
 
     # Define input vector x
     x = None
+
 
     for j in range(0,N-1):
         try:
@@ -50,9 +58,22 @@ for i in range(N-1,len(word_list)):
         except:
             x = w2v[word_list[j-N+1]]
 
+    # Done with defining  x
+
     np.reshape(x,(len(x),1))
-    x = torch.FloatTensor(x)
+    x = Variable(torch.FloatTensor(x))
     optimizer.zero_grad()
     outputs = mlp(x)
-    print (outputs)
-    #target = w2v[word_list[i]]
+
+    index = w2i[word_list[i]]
+    one_hot_target = torch.zeros(num_classes,1)
+
+    #print ('index is',index)
+    one_hot_target[index] = 1
+    #print (one_hot_target)
+
+    # loss = criterion(outputs,target)
+    #loss.backward()
+    #optimizer.step()
+
+
