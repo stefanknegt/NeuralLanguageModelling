982876125ebdec3758d3e5b46b9e7c483cd2e68c
Unknown <tom@vd.nl>
2017-11-15 14:46:10 +0100

    KenLM for perplexity baseline

---
+++ KenLM_baseline/perplexity.py
@@ -0,0 +1,18 @@
+import kenlm
+import numpy as np
+import math
+
+m = kenlm.Model("train4.arpa")
+
+per_file = "../penn/train.txt"
+data = []
+for line in open(per_file, 'r'):
+    for word in line.split():
+        #if word != "<unk>":
+        data.append(word)
+s = " ".join(data)
+
+perpl = m.perplexity(s) #This should work with kenlm.Model. But this is not working now...
+print(perpl)
+
+#cat train.txt| python ../KenLM/process.py | ../../../../../kenlm/bin/lmplz -o 3 > train.arpa
