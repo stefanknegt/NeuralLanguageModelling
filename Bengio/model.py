4858663504abc47c7df659d89d323ddb99f6c9ac
jorism1993 <jorismollinga@hotmail.com>
2017-11-27 12:21:27 +0100

    ssh update

--- Bengio/model.py
+++ Bengio/model.py
@@ -89,6 +89,7 @@ def calculate_perplexity(N, word_list, w2i, trained_model):
             sentence_prob += output[0][required_index].data[0]
 
         test_set_prob += np.log2(np.exp(sentence_prob))
+        
 
     number_of_words = len(word_list) - (len(sentence_list) * (N - 1))
     perplexity = 2 ** ((-1 / number_of_words) * test_set_prob)
