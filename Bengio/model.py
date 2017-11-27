d14e6fdaf2a9645d4f021fd33684998afa6a1b47
jorism1993 <jorismollinga@hotmail.com>
2017-11-27 14:13:45 +0100

    update200

--- Bengio/model.py
+++ Bengio/model.py
@@ -55,7 +55,7 @@ def train(N,num_epochs,ngram,w2i,mlp):
             total_loss += loss.data
             iter_count += 1
 
-            if iter_count % 100 == 0:
+            if iter_count % 1000 == 0:
                 print ('in epoch',epoch,'it is now at iter',100.0*iter_count/len(ngram),'%')
         print('After epoch', epoch, 'the total loss is', total_loss)
     return mlp
