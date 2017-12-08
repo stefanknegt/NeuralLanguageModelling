20043105771c22d67a8a85cd1187ab6f3efd418a
Stefan Knegt <stefanknegt@gmail.com>
2017-12-08 12:27:41 +0100

    Load test sentences and run script

--- RAN/supervised_generate.py
+++ RAN/supervised_generate.py
@@ -61,42 +61,47 @@ input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
 if args.cuda:
     input.data = input.data.cuda()
 
-sentence = "mrs. hills said many of the N countries that she placed on the <unk> are great"
-sentence_list = sentence.split(' ')
-idx_list = [corpus.dictionary.word2idx[s] for s in sentence_list]
+with open('data/penn/test.txt') as f:
+    sentences = f.readlines()
+sentences = [x.strip() for x in sentences]
+for sentence in sentences:
+    #sentence = "mrs. hills said many of the N countries that she placed on the <unk> are great"
+    sentence_list = sentence.split(' ')
+    print(sentence_list)
+    idx_list = [corpus.dictionary.word2idx[s] for s in sentence_list]
 
-settings.init()
-hidden = model.init_hidden(1)
-input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
-input.data.fill_(idx_list[0])
-for i in range(len(sentence_list)):
-    output, hidden = model.forward(input, hidden)
-    word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
-    if i < len(sentence_list)-1:
-        word_idx = idx_list[i+1]
-        input.data.fill_(word_idx)
+    settings.init()
+    hidden = model.init_hidden(1)
+    input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
+    input.data.fill_(idx_list[0])
+    for i in range(len(sentence_list)):
+        output, hidden = model.forward(input, hidden)
+        word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
+        if i < len(sentence_list)-1:
+            word_idx = idx_list[i+1]
+            input.data.fill_(word_idx)
 
-    assert(len(settings.iList) == len(settings.fList))
-    word_list = np.zeros((len(settings.iList),len(settings.iList)))
-    num_words = len(settings.iList)
-    for word in range(0,num_words):
-        for depth in range(0,num_words):
-            if (word>depth):
-                forget = settings.fList[depth+1]
-                for k in range(depth+2,word+1):
-                    forget = forget*settings.fList[k]
-                answer = (torch.mm(settings.iList[depth],forget.transpose(0,1)))
-                answer = answer.data.numpy()
-                word_list[depth][word] = answer
-    mean_distance = np.zeros(num_words)
-    for i in range(1,num_words):
-        average_index = 0
-        for j in range(0,num_words):
-            average_index += word_list[j][i] * j
-        mean_distance[i] = i - average_index / np.sum(word_list[:,i])
+        assert(len(settings.iList) == len(settings.fList))
+        word_list = np.zeros((len(settings.iList),len(settings.iList)))
+        num_words = len(settings.iList)
+        for word in range(0,num_words):
+            for depth in range(0,num_words):
+                if (word>depth):
+                    forget = settings.fList[depth+1]
+                    for k in range(depth+2,word+1):
+                        forget = forget*settings.fList[k]
+                    answer = (torch.mm(settings.iList[depth],forget.transpose(0,1)))
+                    answer = answer.data.numpy()
+                    word_list[depth][word] = answer
+        mean_distance = np.zeros(num_words)
+        for i in range(1,num_words):
+            average_index = 0
+            for j in range(0,num_words):
+                average_index += word_list[j][i] * j
+            mean_distance[i] = i - average_index / np.sum(word_list[:,i])
 
-    max_dependency = np.argmax(word_list, axis=0)
-    distance =  np.arange(0,num_words) - max_dependency
-    word_dependency = [sentence_list[i] for i in max_dependency]
+        max_dependency = np.argmax(word_list, axis=0)
+        distance =  np.arange(0,num_words) - max_dependency
+        word_dependency = [sentence_list[i] for i in max_dependency]
 
-print(sentence_list, idx_list, word_dependency, max_dependency, distance)
+    print(sentence_list, idx_list, word_dependency, max_dependency, distance)
